d95028cb40be0b26f14035eb7b801c38cac183d9
pks9862728888 <pks9862728888@gmail.com>
2025-08-20 21:44:48 +0530

    added few shot prompt template

--- langchaindemo/app/prompt_templates.py
+++ langchaindemo/app/prompt_templates.py
@@ -1,4 +1,5 @@
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import PromptTemplate, \
+ChatPromptTemplate, MessagesPlaceholder, FewShotPromptTemplate
 from app.model import model
 
 
@@ -42,3 +43,47 @@ def messages_placeholder(messages_to_pass):
     })
     chat_with_model(formatted_prompt)
 
+
+def few_shot_prompt_template():
+    """
+    Used to create a few-shot prompt with multiple examples
+    """
+    example_prompt = PromptTemplate.from_template("Question: {question}\n{answer}")
+    examples = get_example_set()
+    # print(example_prompt.invoke(examples[0]).to_string())
+    prompt = FewShotPromptTemplate(
+        examples = examples,
+        example_prompt = example_prompt,
+        suffix = "Question: {input}",
+        input_variables = ["input"]
+    )
+    formatted_prompt = prompt.invoke({"input": "Who is the father of computer?"})
+    print(formatted_prompt.to_string())
+    chat_with_model(formatted_prompt)
+
+
+def get_example_set() -> list[dict]:
+    return [
+        {
+            "question": "Who lived longer, Muhammad Ali or Alan Turing?",
+            "answer": """
+Are follow up questions needed here: Yes.
+Follow up: How old was Muhammad Ali when he died?
+Intermediate answer: Muhammad Ali was 74 years old when he died.
+Follow up: How old was Alan Turing when he died?
+Intermediate answer: Alan Turing was 41 years old when he died.
+So the final answer is: Muhammad Ali
+""",
+        },
+        {
+            "question": "When was the founder of craigslist born?",
+            "answer": """
+Are follow up questions needed here: Yes.
+Follow up: Who was the founder of craigslist?
+Intermediate answer: Craigslist was founded by Craig Newmark.
+Follow up: When was Craig Newmark born?
+Intermediate answer: Craig Newmark was born on December 6, 1952.
+So the final answer is: December 6, 1952
+""",
+        }
+    ]
--- langchaindemo/main.py
+++ langchaindemo/main.py
@@ -1,15 +1,16 @@
 from app.config import settings
-from app.prompt_templates import chat_prompt_template, chat_with_model, messages_placeholder, string_prompt_template
+from app.prompt_templates import chat_prompt_template, chat_with_model, few_shot_prompt_template, messages_placeholder, string_prompt_template
 from langchain_core.messages import HumanMessage, AIMessage
 
 def main():
     # string_prompt_template("computer")
     # chat_prompt_template("computer")
-    messages_placeholder([
-        HumanMessage("What's your favorite programming language?"),
-        AIMessage("Ayey mate, mine is Python!"),
-        HumanMessage("So tell me something pythony??")
-    ])
+    # messages_placeholder([
+    #     HumanMessage("What's your favorite programming language?"),
+    #     AIMessage("Ayey mate, mine is Python!"),
+    #     HumanMessage("So tell me something pythony??")
+    # ])
+    few_shot_prompt_template()
 
 
 if __name__ == "__main__":
